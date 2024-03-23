a0cd336dc7ac9a7cfcadbe413ce331d6ffea5309
divyam137 <121971400+divyam137@users.noreply.github.com>
2024-03-23 23:35:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let boxes = document.querySelectorAll(".box");
+let resetb = document.querySelector(".reset");
+let win=document.querySelector("#win");
+let winb=document.querySelector("#winb");
+let msg=document.querySelector(".msg")
+let turnofx=true;
+resetb.classList.remove("hide1");
+
+const winpattern = [
+    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]
+];
+const resetg=() =>{
+    turnofx=true;
+    for(let box of boxes){
+    box.disabled=false; 
+    box.innerText="";}
+    msg.classList.add("hide");
+    resetb.classList.remove("hide1");
+
+
+}; 
+boxes.forEach((box)=>{
+    box.addEventListener ( "click",()=>{
+        console.log("box was clicked");
+        if (turnofx){
+        box.innerText = "X";
+        box.style.color="red";
+    turnofx=false;}
+    else 
+    {box.innerText = "O";
+    box.style.color = "green";
+     turnofx=true;}
+   box.disabled=true;
+   checkwin();
+
+    });
+});
+const show = (pos1) => {
+    if(pos1 == "X")
+   { win.innerText= 'Congrats, winner is X';}
+else 
+if(pos1 == "O")
+{win.innerText= "Congrats, winner is O";}
+   
+    msg.classList.remove("hide");
+    
+ 
+};
+const checkwin=() => {
+    for(let pattern of winpattern )
+    {
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2= boxes[pattern[1]].innerText; 
+        let pos3= boxes[pattern[2]].innerText;   
+        if(pos1!="" && pos2!="" && pos3!="")
+        {
+            if(pos1==pos2 && pos2==pos3 && pos3==pos1)
+            {console.log("Winner is", pos1);
+            resetb.classList.add("hide1")
+        show (pos1);
+        for(let box of boxes){
+            box.disabled=true;
+           
+        }}
+        
+        }
+       
+    }
+};
+winb.addEventListener("click", resetg);
+
+resetb.addEventListener("click", resetg);
